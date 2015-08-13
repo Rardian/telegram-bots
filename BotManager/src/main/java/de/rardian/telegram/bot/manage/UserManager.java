591431e9f5b6aa729d81fa37d3e0a7f433ecd93c
Rardian <Rardian@192.168.54.19>
2015-08-13 21:33:39 +0200

    added tests for UserManager registerUser throws NPE on null-user

--- BotManager/src/main/java/de/rardian/telegram/bot/manage/UserManager.java
+++ BotManager/src/main/java/de/rardian/telegram/bot/manage/UserManager.java
@@ -3,6 +3,8 @@ package de.rardian.telegram.bot.manage;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.commons.lang3.Validate;
+
 import de.rardian.telegram.bot.model.User;
 
 public class UserManager {
@@ -13,6 +15,7 @@ public class UserManager {
 	}
 
 	public void registerUser(User user) {
+		Validate.notNull(user);
 		knownUsers.add(user);
 	}
 
---
+++ BotManager/src/test/java/de/rardian/telegram/bot/manage/UserManagerTest.java
@@ -0,0 +1,87 @@
+package de.rardian.telegram.bot.manage;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import de.rardian.telegram.bot.model.User;
+
+public class UserManagerTest {
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void type() throws Exception {
+		assertThat(UserManager.class, notNullValue());
+	}
+
+	@Test
+	public void instantiation() throws Exception {
+		UserManager target = new UserManager();
+		assertThat(target, notNullValue());
+	}
+
+	@Test
+	public void isUserKnown_userUnknown() throws Exception {
+		// Init
+		UserManager underTest = new UserManager();
+
+		// Run
+		boolean result = underTest.isUserKnown(User.TEST_USER);
+
+		// Assert
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void isUserKnown_userKnown() throws Exception {
+		// Init
+		UserManager underTest = new UserManager();
+		underTest.registerUser(User.TEST_USER);
+
+		// Run
+		boolean result = underTest.isUserKnown(User.TEST_USER);
+
+		// Assert
+		assertThat(result, is(true));
+	}
+
+	@Test
+	public void isUserKnown_userNull() throws Exception {
+		// Init
+		UserManager underTest = new UserManager();
+
+		// Run
+		boolean result = underTest.isUserKnown(null);
+
+		// Assert
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void registerUser() throws Exception {
+		// Init
+		UserManager underTest = new UserManager();
+
+		// Run
+		underTest.registerUser(User.TEST_USER);
+
+		// Assert
+		assertThat(underTest.isUserKnown(User.TEST_USER), is(true));
+	}
+
+	@Test
+	public void registerUser_userNull() throws Exception {
+		// Init
+		thrown.expect(NullPointerException.class);
+
+		UserManager underTest = new UserManager();
+
+		// Run
+		underTest.registerUser(null);
+	}
+}
