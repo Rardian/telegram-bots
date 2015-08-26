5e84c3ed4e21b5fb88d202329df1ecb57fcd8aa4
Rardian <Rardian@192.168.54.19>
2015-08-26 21:15:50 +0200

    tests added

---
+++ src/test/java/de/rardian/telegram/bot/command/ActionExecuterTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package de.rardian.telegram.bot.command;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import de.rardian.telegram.bot.manage.Message;
+
+@RunWith(org.mockito.runners.MockitoJUnitRunner.class)
+public class ActionExecuterTest {
+	@Mock
+	ActionInitializer actionInitializer;
+	@Mock
+	Action action;
+	@Mock
+	Message message;
+
+	private ActionExecuter underTest;
+
+	@Before
+	public void initExecuter() {
+		underTest = new ActionExecuter().withInitializer(actionInitializer);
+	}
+
+	@Test
+	public void execute() throws Exception {
+		// Init
+
+		// Run
+		underTest.execute(action, message);
+
+		// Assert
+		verify(actionInitializer).injectActionDependencies(action, message);
+		verify(action).execute();
+	}
+
+}
