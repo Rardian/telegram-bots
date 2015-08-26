e41046cda4b4798eddf4339c3b1269bbb511db55
Rardian <Rardian@192.168.54.19>
2015-08-26 22:16:45 +0200

    Test added

---
+++ src/test/java/de/rardian/telegram/bot/castle/commands/actions/MoveInhabitantToProductionActionTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package de.rardian.telegram.bot.castle.commands.actions;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import de.rardian.telegram.bot.castle.model.Castle;
+import de.rardian.telegram.bot.model.User;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MoveInhabitantToProductionActionTest {
+	@Mock
+	private Castle castle;
+	@Mock
+	private User user;
+	@InjectMocks
+	private MoveInhabitantToProductionAction underTest;
+
+	@Test
+	public void execute() throws Exception {
+		// Run
+		underTest.execute();
+
+		// Assert
+		verify(castle).addProducer(user);
+	}
+
+}
