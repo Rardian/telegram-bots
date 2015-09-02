425bf52624ff69e7d500b4223998311f29a46890
Rardian <Rardian@192.168.54.19>
2015-09-02 22:39:59 +0200

    PrintHelpAction
    test added

---
+++ src/test/java/de/rardian/telegram/bot/castle/commands/actions/PrintHelpActionTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package de.rardian.telegram.bot.castle.commands.actions;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import de.rardian.telegram.bot.command.MessageReply;
+import de.rardian.telegram.bot.model.Bot;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PrintHelpActionTest {
+	private static final String OVERVIEW_TEST = "TESTOVERVIEW";
+	@Mock
+	private Bot bot;
+	@Mock
+	private MessageReply reply;
+	@InjectMocks
+	private PrintHelpAction underTest;
+
+	@Test
+	public void executeSendsCommandOverview() throws Exception {
+		// Init
+		when(bot.getCommandOverview()).thenReturn(OVERVIEW_TEST);
+
+		// Run
+		underTest.execute();
+
+		// Assert
+		verify(reply).answer(OVERVIEW_TEST, null);
+	}
+}
