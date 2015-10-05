bc94dfd82ff28289ef99d198d29a4e306ef820a8
bjoern.osbahr <bjoern.osbahr@teufel.de>
2015-10-05 15:51:43 +0200

    CastleBot - EnvironmentFacility
    - added broadcast on finding a resource field

--- src/main/java/de/rardian/telegram/bot/castle/facilities/EnvironmentFacility.java
+++ src/main/java/de/rardian/telegram/bot/castle/facilities/EnvironmentFacility.java
@@ -2,16 +2,22 @@ package de.rardian.telegram.bot.castle.facilities;
 
 import static de.rardian.telegram.bot.castle.facilities.CastleFacilityCategories.SCOUTING;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.Validate;
 
 import de.rardian.telegram.bot.castle.model.Castle;
 import de.rardian.telegram.bot.castle.model.Inhabitant;
 import de.rardian.telegram.bot.castle.model.Resources;
+import de.rardian.telegram.bot.command.action.BroadcastMessageAction;
 import de.rardian.telegram.bot.model.Bot;
+import de.rardian.telegram.bot.model.User;
 
 public class EnvironmentFacility extends BasicFacility implements Runnable {
 
@@ -33,7 +39,8 @@ public class EnvironmentFacility extends BasicFacility implements Runnable {
 
 	@Override
 	public ProcessResult2 process() {
-		// TODO based on SCOUTING skill and already existing resource locations the chance for finding new spots de- or increases
+		// TODO based on SCOUTING skill and already existing resource locations the chance for finding new spots de- or
+		// increases
 		ProcessResult2 resultContainer = new ProcessResult2();
 
 		synchronized (members) {
@@ -45,12 +52,21 @@ public class EnvironmentFacility extends BasicFacility implements Runnable {
 				if (scoutingSkill * 10 > resourceFieldCount) {
 					System.out.println(inhabitant.getName() + " erhöht fieldCount");
 					resources.increaseResourceFieldCount();
+					if (resources.getResourceFieldCount() > resourceFieldCount) {
+						User user = castle.getUserBy(inhabitant);
+						Collection<Inhabitant> otherMembers = CollectionUtils.disjunction(members, Arrays.asList(inhabitant));
+						resultContainer.addResultAction(//
+								new BroadcastMessageAction(//
+										user, "Du hast ein Ressourcenfeld gefunden.", //
+										new ArrayList<Inhabitant>(otherMembers), inhabitant.getName()
+												+ " hat ein Ressourcenfeld gefunden."));
+					}
 				}
 
-				//			if (resourceFieldCount % 2 == 0) {
+				// if (resourceFieldCount % 2 == 0) {
 
 				super.increaseInhabitantXp(inhabitant, SCOUTING, resultContainer);
-				//			}				
+				// }
 			}
 		}
 
