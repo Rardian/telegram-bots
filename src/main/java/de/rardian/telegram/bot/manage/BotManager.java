3dab53faf96d053b4cab48ea78a6fbb233c72aec
Rardian <Rardian@192.168.54.19>
2016-02-02 21:01:43 +0100

    CastleBot - Persistence
    Finally persistence! :)
    Currently only User data is stored, so User wouldn't get welcome message
    twice.

--- src/main/java/de/rardian/telegram/bot/manage/BotManager.java
+++ src/main/java/de/rardian/telegram/bot/manage/BotManager.java
@@ -1,7 +1,6 @@
 package de.rardian.telegram.bot.manage;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
@@ -12,28 +11,28 @@ import de.rardian.telegram.bot.model.User;
 import de.rardian.telegram.bot.model.UserRepository;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
-@EntityScan(basePackageClasses=User.class)
+@EntityScan(basePackageClasses = User.class)
 public class BotManager {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BotManager.class);
-//		new BotManager().start();
+		//		new BotManager().start();
 	}
 
 	@Bean
 	public UpdatesRetriever startBot(UserManager userManager) {
 		final CastleBot bot = new CastleBot();
 		bot.setUserManager(userManager);
-		
+
 		final UpdatesRetriever retriever = new UpdatesRetriever().forBot(bot);
 		retriever.startGettingUpdates();
-		
+
 		return retriever;
 	}
-	
-	@Bean 
+
+	@Bean
 	public UserManager createUserManager(UserRepository userRepository) {
 		UserManager userManager = new UserManager();
 		userManager.setUserRepository(userRepository);
