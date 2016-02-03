084f7aad72288046654d3fa58e1ecbe71364f0f8
Rardian <Rardian@192.168.54.19>
2016-02-03 20:31:58 +0100

    Formatting

--- src/main/java/de/rardian/telegram/bot/manage/UserManager.java
+++ src/main/java/de/rardian/telegram/bot/manage/UserManager.java
@@ -1,7 +1,5 @@
 package de.rardian.telegram.bot.manage;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 
 import org.apache.commons.lang3.Validate;
@@ -17,54 +15,54 @@ import de.rardian.telegram.bot.model.User;
 import de.rardian.telegram.bot.model.UserRepository;
 
 public class UserManager {
-//	private Collection<User> knownUsers = new ArrayList<>();
+	//	private Collection<User> knownUsers = new ArrayList<>();
 	private UserRepository userRepository;
 	private InhabitantRepository inhabitantRepository;
 
 	public void setUserRepository(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
-	
+
 	public void setInhabitantRepository(InhabitantRepository inhabitantRepository) {
 		this.inhabitantRepository = inhabitantRepository;
 	}
-/*
-	private boolean collectionContainsUser(Collection<User> collection, User user) {
-		return collectionContains(collection, user, new UserByIdPredicate(user));
-	}
-*/
+	/*
+		private boolean collectionContainsUser(Collection<User> collection, User user) {
+			return collectionContains(collection, user, new UserByIdPredicate(user));
+		}
+	*/
 	/*
 	public static <O> boolean collectionContains(Collection<O> collection, O user, Predicate<O> predicate) {
 		return CollectionUtils.exists(collection, predicate);
 	}
 	*/
 
-	public Collection<Inhabitant> getInhabitants(){
-		return Lists.newArrayList(inhabitantRepository.findAll()); 
+	public Collection<Inhabitant> getInhabitants() {
+		return Lists.newArrayList(inhabitantRepository.findAll());
 	}
-	
-	public long countInhabitants(){
+
+	public long countInhabitants() {
 		return inhabitantRepository.count();
 	}
-	
+
 	public boolean isUserKnown(final User user) {
 		Validate.notNull(user);
 		boolean resultRep = userRepository.exists(Long.valueOf(user.getId()));
-	//	boolean resultCol = collectionContainsUser(knownUsers, user);
-//		System.out.println("Repo=" + resultRep + ", Coll=" + resultCol);
+		//	boolean resultCol = collectionContainsUser(knownUsers, user);
+		System.out.println("userKnown=" + resultRep);
 		return resultRep;
 		//		return collectionContainsUser(knownUsers, user);
 	}
 
 	public Action registerUser(User user) {
 		Validate.notNull(user);
-//		knownUsers.add(user);
-//		User storedUser = userRepository.save(user);
+		//		knownUsers.add(user);
+		//		User storedUser = userRepository.save(user);
 
-//		String logName = (storedUser.getUserName() == null ? storedUser.getFirstName() : storedUser.getUserName());
-//		System.out.println("User '" + logName + "' registered");
-		
-		if(isUserKnown(user)){
+		//		String logName = (storedUser.getUserName() == null ? storedUser.getFirstName() : storedUser.getUserName());
+		//		System.out.println("User '" + logName + "' registered");
+
+		if (isUserKnown(user)) {
 			return new DoNothingAction();
 		} else {
 			Inhabitant newInhabitant = new Inhabitant();
@@ -73,8 +71,7 @@ public class UserManager {
 
 			return new UserMovesInAction();
 		}
-	
-		
+
 	}
 
 	public Inhabitant getInhabitantByUser(User user) {
